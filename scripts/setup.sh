3af6c69571d25350e830e07e93839e9d5523b30f
Anil Madhavapeddy <anil@recoil.org>
2026-02-22 13:26:55 +0000

    sandbox

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,8 +4,8 @@ export OPAMROOT=`pwd`/_opam
 export OPAMYES=1
 
 if [ ! -d _opam ]; then
-  opam init -any --bare
-fi
+  opam init -any --bare --disable-sandboxing
+fi 
 
 if [ ! -d _local ]; then
   opam switch create . --repos lox=git+https://github.com/oxcaml/opam-repository.git,default -y
