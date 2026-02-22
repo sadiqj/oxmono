2e6fbccd55b21cb3d5c107ad1b920ebddcecc694
Anil Madhavapeddy <anil@recoil.org>
2026-02-22 13:22:38 +0000

    init

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,12 @@
 #!/bin/sh -ex
 
+export OPAMROOT=`pwd`/_opam
+export OPAMYES=1
+
+if [ ! -d _opam ]; then
+  opam init -any --bare
+fi
+
 if [ ! -d _local ]; then
   opam switch create . --repos lox=git+https://github.com/oxcaml/opam-repository.git,default -y
 fi
