95fd3c75c2f04bffd5272e5673acced917259813
Anil Madhavapeddy <anil@recoil.org>
2026-02-23 13:47:05 +0000

    deniug

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -11,4 +11,9 @@ if [ ! -d _local ]; then
   opam switch create . --repos lox=git+https://github.com/oxcaml/opam-repository.git,default -y
 fi
 
+# debug
+opam exec -- dune build opam/base/src/mpopcnt.sexp --verbose
+echo mpopcnt sexp:
+cat _build/default/opam/base/src/mpopcnt.sexp
+echo resuming build:
 opam exec -- dune build --profile=release @install
